16a220d20f6cf573bbc4578ac8f77f3dbcc6ba6c
Xavier Ruiz <xavierruiz@Xaviers-MacBook-Pro.local>
2025-05-12 20:39:36 -0500

    Setup basic word count map reduce job

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # tutorial-hadoop-word-count
+
+# Installation of Hadoop
+```brew install hadoop```
+https://medium.com/@yashaswee/installing-hadoop-in-macos-2025-all-errors-resolved-bfc7522be39f
+
+Add the following to zshrc:
+export PATH="$PATH:/opt/homebrew/opt/hadoop/libexec/bin"
+export HADOOP_MAPRED_HOME=/opt/homebrew/opt/hadoop/libexec
+
+# Hadoop Architecture
+https://labex.io/tutorials/hadoop-how-to-resolve-connecting-to-resourcemanager-at-0-0-0-0-8032-error-in-hadoop-415647
+
--- src/main/java/com/xruiz/wordcount/App.java
+++ src/main/java/com/xruiz/wordcount/App.java
@@ -1,10 +1,29 @@
 package com.xruiz.wordcount;
 
-/**
- * Hello world!
- */
+import java.io.IOException;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapred.FileInputFormat;
+import org.apache.hadoop.mapred.FileOutputFormat;
+import org.apache.hadoop.mapred.JobClient;
+import org.apache.hadoop.mapred.JobConf;
+import org.apache.hadoop.mapred.TextInputFormat;
+import org.apache.hadoop.mapred.TextOutputFormat;
+
 public class App {
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
+    public static void main(String[] args) throws IOException{
+        JobConf conf = new JobConf(App.class);
+        conf.setJobName("WordCount");
+        conf.setOutputKeyClass(Text.class);
+        conf.setOutputValueClass(IntWritable.class);
+        conf.setMapperClass(WordCountMapper.class);
+        conf.setCombinerClass(WordCountReducer.class);
+        conf.setReducerClass(WordCountReducer.class);
+        conf.setInputFormat(TextInputFormat.class);
+        conf.setOutputFormat(TextOutputFormat.class);
+        FileInputFormat.setInputPaths(conf,new Path(args[0]));
+        FileOutputFormat.setOutputPath(conf,new Path(args[1]));
+        JobClient.runJob(conf);
     }
 }
